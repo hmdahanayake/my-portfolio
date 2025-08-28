d03f47c1e7678eec127d0390f1705dbf8633e694
HasiniDahanayake2020 <hasini.m.dahanayake@gmail.com>
2025-08-28 14:46:23 +0530

    remove  portfolio images with correct filenames

--- src/components/testimonials/Data.jsx
+++ src/components/testimonials/Data.jsx
@@ -1,6 +1,6 @@
 /*import Image1 from "../../assets/image1.png";
-import Image2 from "../../assets/image2.png";*/
-import Image3 from "../../assets/image3.jpg";
+import Image2 from "../../assets/image2.png";
+import Image3 from "../../assets/image3.jpg";*/
 
 
 
@@ -8,19 +8,19 @@ export const Data = [
    
     {
         id:1,
-       image:Image3,
+       /*image:Image3,*/
         title:"R. Pradeep",
         description:"Hasini designed our vehicle company's landing page with incredible attention to detail. The UI is modern, responsive, and engaging. We’ve received fantastic feedback from our clients, and our online presence has significantly improved thanks to her expertise in frontend development and UI/UX design.",
     },
     {
         id:2,
-         image:Image3,
+         /*image:Image3,*/
         title:"Emily Disanayake",
         description:"We hired Hasini to create our photography website, and the results were amazing. The site perfectly showcases our portfolio with elegant design and smooth UX. Her work helped us attract more clients and elevate our brand identity online.",
     },
     {
         id:3,
-        image:Image3,
+        /*image:Image3,*/
         title:"L. Dahanayake",
         description:"Hasini developed our AI-powered travel planning website from scratch. The application is intuitive, responsive, and visually appealing. She integrated both frontend and backend seamlessly, making the platform user-friendly and highly functional.",
     },
