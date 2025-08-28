a8b585ea84b8314bc18ad8a8f81abf48b6ddeea9
HasiniDahanayake2020 <hasini.m.dahanayake@gmail.com>
2025-08-28 14:53:29 +0530

    add  img  filenames

--- src/components/testimonials/Data.jsx
+++ src/components/testimonials/Data.jsx
@@ -1,6 +1,6 @@
-import Work1 from '../../assets/work1.jpg'
-import Work2 from '../../assets/work2.jpg'
-import Work3 from '../../assets/work3.jpg'
+import Work1 from '../../assets/image1.png'
+import Work2 from '../../assets/image2.png'
+import Work3 from '../../assets/image3.jpg'
 
 
 
@@ -10,17 +10,17 @@ export const Data = [
         id:1,
        image:Work1,
         title:"R. Pradeep",
-        description:"Hasini designed our vehicle company's landing page with incredible attention to detail. The UI is modern, responsive, and engaging. We’ve received fantastic feedback from our clients, and our online presence has significantly improved thanks to her expertise in frontend development and UI/UX design.",
+        description:"Hasini designed our vehicle company's landing page with incredible attention to detail. The UI is modern, responsive, and engaging. We’ve received fantastic feedback from our clients, and our online presence has significantly improved thanks.",
     },
     {
         id:2,
-         image:Work1,
+         image:Work2,
         title:"Emily Disanayake",
         description:"We hired Hasini to create our photography website, and the results were amazing. The site perfectly showcases our portfolio with elegant design and smooth UX. Her work helped us attract more clients and elevate our brand identity online.",
     },
     {
         id:3,
-        image:Work1,
+        image:Work3,
         title:"L. Dahanayake",
         description:"Hasini developed our AI-powered travel planning website from scratch. The application is intuitive, responsive, and visually appealing. She integrated both frontend and backend seamlessly, making the platform user-friendly and highly functional.",
     },
